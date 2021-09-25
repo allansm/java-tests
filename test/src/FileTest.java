8ec01d81530c0dade31a23ae4068a724b62d0e86
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-09-25 16:54:09 -0300

    ...

--- test/src/FileTest.java
+++ test/src/FileTest.java
@@ -3,7 +3,9 @@ package test;
 import  java.io.FileOutputStream;
 import  java.nio.file.Files;
 import  java.nio.file.Paths;
-import java.util.List;
+import  java.util.List;
+import  java.io.File;
+import  java.util.Arrays;
 
 public class FileTest{
 	public static void writeFile(String fname,String txt){
@@ -26,11 +28,19 @@ public class FileTest{
 		return text;
 	}
 
+	public static List<String> dir(String path){
+		return Arrays.asList(new File(path).list());
+	}
+
 	public static void main(String[]args){	
 		writeFile("test","helloworld");
 		
 		for(String line : readFile("test")){
 			System.out.println(line);
+		}
+
+		for(String fn : dir("../..")){
+			System.out.println(fn);
 		}	
 	}
 }
