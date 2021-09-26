a43963f29a0ede1b663585df47d0f767ff36ba93
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-09-26 19:46:41 -0300

    ...

--- test/src/FileTest.java
+++ test/src/FileTest.java
@@ -6,6 +6,7 @@ import  java.nio.file.Paths;
 import  java.util.List;
 import  java.io.File;
 import  java.util.Arrays;
+import 	java.nio.file.Path;
 
 public class FileTest{
 	public static void writeFile(String fname,String txt){
@@ -32,15 +33,39 @@ public class FileTest{
 		return Arrays.asList(new File(path).list());
 	}
 
+	public static boolean remove(String fname){
+		return new File(fname).delete();
+	}
+
+	public static String getcwd(){
+		Path cp = Paths.get("");
+		return cp.toAbsolutePath().toString();	
+	}
+
 	public static void main(String[]args){	
 		writeFile("test","helloworld");
 		
+		System.out.println("--------------");
+
 		for(String line : readFile("test")){
 			System.out.println(line);
 		}
-
-		for(String fn : dir("../..")){
+		
+		System.out.println("--------------");	
+		
+		for(String fn : dir(".")){
 			System.out.println(fn);
-		}	
+		}
+
+		remove("test");
+		
+		System.out.println("--------------");
+		
+		for(String fn : dir(".")){
+			System.out.println(fn);
+		}
+
+		System.out.println("");
+		System.out.println("current dir:"+getcwd());
 	}
 }
