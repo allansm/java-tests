1f9ee1402eb6300da4a0ade10f19b2d93a8d19c0
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-09-28 17:45:21 -0300

    ...

--- test/src/TextTest.java
+++ test/src/TextTest.java
@@ -1,15 +1,32 @@
 package test;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class TextTest{
 	public static boolean has(String fulltxt,String txt){
 		return fulltxt.contains(txt);
 	}
 
+	public static List<String> explode(String txt,String del){
+		return Arrays.asList(txt.split(del));
+	}
+
+	public static String replace(String fulltxt,String toreplace,String newtxt){
+		return fulltxt.replace(toreplace,newtxt);
+	}
+
 	public static void main(String[]args){
-		String text = "helloworld";
+		String text = "hello-world";
 
 		if(has(text,"world")){
 			System.out.println("contain");
 		}
+
+		for(String n : explode(text,"-")){
+			System.out.println(n);
+		}
+
+		System.out.println(replace(text,"-",""));
 	}
 }
