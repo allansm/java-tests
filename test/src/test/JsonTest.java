e9d909bbabe65e244668389faa8082e3cf1c0947
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-11-17 20:05:59 -0300

    ...

--- test/src/test/JsonTest.java
+++ test/src/test/JsonTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.json.JSONObject;
+import org.json.JSONArray;
 import allansm.file.FileHandle;
 
 public class JsonTest{	
@@ -10,6 +11,17 @@ public class JsonTest{
 		System.out.println(obj.toString());
 		System.out.println(obj.getString("txt"));
 		System.out.println(obj.getInt("n")+obj.getInt("n"));
-			
+		
+		System.out.println(obj.getJSONArray("arr").getInt(2));
+		
+		JSONObject o = obj.getJSONObject("object");
+		System.out.println(o.getString("msg"));
+		System.out.println(o.getString("msg2"));
+		
+		JSONArray a = o.getJSONArray("arr");
+		System.out.println(a.getInt(2)+a.getInt(0));	
+		for(int i=0;i<a.length();i++){
+			System.out.println(a.getInt(i));
+		}	
 	}
 }
