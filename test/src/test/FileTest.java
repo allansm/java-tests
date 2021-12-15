81b486b30d4d21b327177515369a0da6341cad97
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2021-12-15 13:09:06 -0300

    ...

--- test/src/test/FileTest.java
+++ test/src/test/FileTest.java
@@ -7,100 +7,20 @@ import  java.util.List;
 import  java.io.File;
 import  java.util.Arrays;
 import 	java.nio.file.Path;
+import java.util.Scanner;
 
 public class FileTest{
-	public static void writeFile(String fname,String txt){
-		try{
-			FileOutputStream writer = new FileOutputStream(fname);
-				
-			writer.write(txt.getBytes());
-			writer.close();
-		}catch(Exception e){}
-	
-	}
-
-	public static List<String> getLines(String fname){
-		List<String> text = null;
+	public static void main(String[]args) throws Exception{
+		System.out.print("file:");
 		
-		try{
-			text = Files.readAllLines(Paths.get(fname));
-		}catch(Exception e){}
+		long b = Files.size(Paths.get(new Scanner(System.in).nextLine()));
+		long kb = b/1024;
+		long mb = kb/1024;
+		long gb = mb/1024;
 
-		return text;
-	}
-
-	public static String readFile(String fname){
-		boolean flag = false;
-		String tmp = "";
-		
-		for(String line : getLines(fname)){
-			if(flag){
-				tmp+="\n";
-			}else{
-				flag = true;
-			}
-
-			tmp+=line;
-		}
-
-		return tmp;
-	}
-
-	public static List<String> dir(String path){
-		return Arrays.asList(new File(path).list());
-	}
-
-	public static boolean remove(String fname){
-		return new File(fname).delete();
-	}
-
-	public static String getcwd(){
-		Path cp = Paths.get("");
-		return cp.toAbsolutePath().toString();	
-	}
-
-	public static boolean isFile(String path){
-		return new File(path).isFile();
-	}
-
-	public static boolean isDir(String path){
-		return new File(path).isDirectory();
-	}
-
-	public static boolean exists(String path){
-		return new File(path).exists();
-	}
-
-	public static String getTemp(){
-		return System.getProperty("java.io.tmpdir");
-	}
-
-	public static void main(String[]args){	
-		writeFile("test","helloworld");
-		
-		System.out.println("--------------");	
-
-		System.out.println(readFile("test"));
-		
-		System.out.println("--------------");	
-		
-		for(String fn : dir(".")){
-			System.out.println(fn);
-		}
-		System.out.println("--------------");
-		System.out.println("test isfile:"+isFile("test"));
-		System.out.println("test isdir:"+isDir("test"));
-		System.out.println("test exists:"+exists("test"));
-		remove("test");
-		System.out.println("test exists:"+exists("test"));	
-		System.out.println("--------------");
-		
-		for(String fn : dir(".")){
-			System.out.println(fn);
-		}
-
-		System.out.println("");
-		System.out.println("current dir:"+getcwd());
-		System.out.println("temp:"+getTemp());
+		System.out.println(b);
+		System.out.println(kb);
+		System.out.println(mb);
+		System.out.println(gb);
 	}
 }
