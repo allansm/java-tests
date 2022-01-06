5c87dc259ed3fb84220193eac7da2034d94e867d
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-01-06 19:03:38 -0300

    ...

--- test/src/test/SocketTest.java
+++ test/src/test/SocketTest.java
@@ -6,41 +6,67 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class SocketTest{
+	private static PrintWriter w;
+	private static Scanner s;
+	
 	public static void test(int port,SocketTest2 st){
 		try{
 			ServerSocket server = new ServerSocket(port);	
 			st.action(server.accept());
+			
+			w.close();
+			s.close();
 			server.close();
 		}catch(Exception e){}		
 	}
 
 	public static void test2(Socket sock,String txt){
 		try{
-			PrintWriter w = new PrintWriter(sock.getOutputStream());
+			w = new PrintWriter(sock.getOutputStream());
 			w.write(txt);
-			w.flush();
-			w.close();	
+			w.flush();	
 		}catch(Exception e){}	
 	}
 
-	public static void test3(Socket sock){
+	public static String test3(Socket sock){
+		String txt = "";
+		
 		try{
-			Scanner s = new Scanner(sock.getInputStream());
+			s = new Scanner(sock.getInputStream());
 			
-			while(s.hasNextLine()){
-				System.out.println(s.nextLine());
+			while(true){
+				String tmp = s.nextLine();
+				txt+=tmp;
+				if(tmp.contains("Cache")){
+					break;
+				}
 			}
 
-			s.close();
 		}catch(Exception e){}
+
+		return txt;
+	}
+
+	public static String test4(){
+		String header = "HTTP/1.1 200 OK";
+		header+="\nContent-Type: text/html;charset=utf-8";
+		header+="\n\n";	
+
+		return header;
 	}
 
 	public static void main(String[]args) throws Exception{
 		while(true){
-			test(8080,(Socket s) ->{	
-				//test3(s);
-				System.out.println("sending...");
-				test2(s,"helloworld");
+			test(8080,(s) ->{	
+				System.out.println(test3(s));
+				
+				System.out.println("sending...");	
+				
+				test2(s,test4());
+				test2(s,"helloworld<br/>");
+				test2(s,"<div style=\"width:100px;height:100px;background:red\"></div>");
+				test2(s,"<div style=\"width:100px;height:100px;background:green\"></div>");
+				test2(s,"<div style=\"width:100px;height:100px;background:blue\"></div>");
 			});
 		}
 	}
