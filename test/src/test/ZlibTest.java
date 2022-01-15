dfce358d3cb769335805a07fea2a9cf017fd54cf
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-01-15 17:44:42 -0300

    ...

--- test/src/test/ZlibTest.java
+++ test/src/test/ZlibTest.java
@@ -1,9 +1,10 @@
 package test;
 
 import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 public class ZlibTest{
-	public static void main(String[]args){
+	public static void main(String[]args) throws Exception{
 		String big = "";
 
 		for(int i =0;i<9999;i++){
@@ -27,6 +28,23 @@ public class ZlibTest{
 		String o = new String(compressed);
 		
 		System.out.println(o.length());
+
+		System.out.println(o);
+		
+		d.end();
+
+		Inflater i = new Inflater();
+
+		i.setInput(compressed,0,compressed.length);
+		
+		cbyte = new byte[Short.MAX_VALUE];
+		size = i.inflate(cbyte);
+		i.end();
+		byte[] decompressed = new byte[size];
+
+		System.arraycopy(cbyte,0,decompressed,0,size);
+
+		o = new String(decompressed);
 
 		System.out.println(o);
 	}
