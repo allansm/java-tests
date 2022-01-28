496603ceae4944a197012fe4bc71c484b74583b2
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-01-28 13:10:50 -0300

    ...

--- test/src/test/FileTest.java
+++ test/src/test/FileTest.java
@@ -4,23 +4,20 @@ import  java.io.FileOutputStream;
 import  java.nio.file.Files;
 import  java.nio.file.Paths;
 import  java.util.List;
-import  java.io.File;
+//import  java.io.File;
 import  java.util.Arrays;
 import 	java.nio.file.Path;
-import java.util.Scanner;
+import  java.util.Scanner;
+import  allansm.file.File;
 
 public class FileTest{
 	public static void main(String[]args) throws Exception{
-		System.out.print("file:");
-		
-		long b = Files.size(Paths.get(new Scanner(System.in).nextLine()));
-		long kb = b/1024;
-		long mb = kb/1024;
-		long gb = mb/1024;
-
-		System.out.println(b);
-		System.out.println(kb);
-		System.out.println(mb);
-		System.out.println(gb);
+		File test = new File("test.txt");
+		test.write("helloworld");
+		Path test1 = Paths.get("test.txt");
+		Files.move(test1, test1.resolveSibling("test1.txt"));
+		test = new File("test1.txt");
+		System.out.println(test.path());
+		test.remove();
 	}
 }
