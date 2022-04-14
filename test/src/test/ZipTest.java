f915e47bb752aa6c2c80193b6db51b20b14e2b72
__Coder__ <allan.de.souza.melo.rj@gmail.com>
2022-04-14 17:52:28 -0300

    Extract a zip

--- test/src/test/ZipTest.java
+++ test/src/test/ZipTest.java
@@ -3,9 +3,10 @@ package test;
 import java.util.zip.*;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileInputStream;
 
 public class ZipTest{
-	public static void main(String[]args) throws Exception{	
+	public static void test() throws Exception{
 		File f = new File("test.zip");
 		
 		ZipOutputStream zip = new ZipOutputStream(new FileOutputStream(f));
@@ -18,5 +19,27 @@ public class ZipTest{
 		zip.closeEntry();
 
 		zip.close();
+	}
+
+	public static void test2() throws Exception{
+		ZipInputStream zip = new ZipInputStream(new FileInputStream(new File("test.zip")));
+		ZipEntry entry = zip.getNextEntry();
+
+		byte[] bytes = new byte[1024];
+		
+		zip.read(bytes);
+		zip.closeEntry();
+
+		zip.close();
+
+		System.out.println(entry.getName());
+		System.out.println(new String(bytes));
+
+		new File("test.zip").delete();
+	}
+
+	public static void main(String[]args) throws Exception{	
+		test();
+		test2();
 	}
 }
